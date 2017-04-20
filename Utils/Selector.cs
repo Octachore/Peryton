feb47312a6d1a7ed7bb003525daef2f9c095a0dc
Octachore <octachore@outlook.com>
2017-04-21 00:00:50 +0200

    add selector

---
+++ Utils/Selector.cs
@@ -0,0 +1,31 @@
+﻿#region License
+// Copyright (c) 2017 Nicolas Maurice (Octachore)
+// Licensed under the terms of the GNU General Public License
+
+
+// This file is part of Peryton.
+
+// Peryton is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License, or
+// any later version.
+
+// Peryton is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Peryton.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+using System;
+using System.Linq;
+
+namespace Utils
+{
+    public static class Selector
+    {
+        public static T Min<T>(Func<T, IComparable> f, params T[] args) => args.OrderBy(t => f(t)).First();
+        public static T Max<T>(Func<T, IComparable> f, params T[] args) => args.OrderByDescending(t => f(t)).First();
+    }
+}
